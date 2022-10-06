9ca3f2719a70ba02c8cff3ef08c9532cfbd7d615
Lorenzo Donadio <lorenzo.donadio@deerns.com>
2022-10-06 17:22:06 +0200

    Update Lineup.svelte

--- src/lib/components/Lineup.svelte
+++ src/lib/components/Lineup.svelte
@@ -2,35 +2,47 @@
 	<a href="#grandescene">
 		<div class="m-2 sm:m-4 border-white rounded-md p-4 border bg-black group bg-opacity-70">
 			<h4 class="sm:text-lg font-semibold group-hover:text-teal-400">GRANDE SCENE</h4>
-			<p class="border-b border-white border-dashed border-opacity-50">Soom T & The Stone Monks</p>
-			<p class="border-b border-white border-dashed border-opacity-50">We are Magonia</p>
-			<p class="border-b border-white border-dashed border-opacity-50">Regal Trip</p>
+			<span class="text-sm -translate-y-1">20h30-21h30</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Regal Trip</p>
+			<span class="text-sm -translate-y-1">22h30-23h30</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Soom T & The Stone Monks</p>
+			<span class="text-sm -translate-y-1">22h30-23h30</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">We are Magonia</p>
 		</div>
 	</a>
 	<a href="#scenesat">
 		<div class="m-2 sm:m-4 border-white rounded-md p-4 border bg-black group bg-opacity-70">
 			<h4 class="sm:text-lg font-semibold group-hover:text-teal-400">SCENE SAT</h4>
-			<p class="border-b border-white border-dashed border-opacity-50">
+			<span class="text-sm -translate-y-1">20h-21h</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Kety Fusco</p>
+			<span class="text-sm -translate-y-1">21h45-22h45</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Archair</p>
+			<span class="text-sm -translate-y-1">23h45-1h</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">
 				Nathalie Froehlich ft. Yanneck
 			</p>
-			<p class="border-b border-white border-dashed border-opacity-50">Kety Fusco</p>
-			<p class="border-b border-white border-dashed border-opacity-50">Archair</p>
 		</div>
 	</a>
 	<a href="#sceneunder">
 		<div class="m-2 sm:m-4 border-white rounded-md p-4 border bg-black group bg-opacity-70">
 			<h4 class="sm:text-lg font-semibold group-hover:text-teal-400">SCENE UNDERGROUND</h4>
-			<p class="border-b border-white border-dashed border-opacity-50">Afterlife</p>
-			<p class="border-b border-white border-dashed border-opacity-50">Mascarpone</p>
-			<p class="border-b border-white border-dashed border-opacity-50">Maissouille</p>
+			<span class="text-sm -translate-y-1">21h15-22h15</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Mascarpone</p>
+			<span class="text-sm -translate-y-1">22h45-00h45</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Afterlife</p>
+			<span class="text-sm -translate-y-1">1h-2h30</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Maissouille</p>
 		</div>
 	</a>
 	<a href="#scenechill">
 		<div class="m-2 sm:m-4 border-white rounded-md p-4 border bg-black group bg-opacity-70">
 			<h4 class="sm:text-lg font-semibold group-hover:text-teal-400">SCENE CHILL</h4>
-			<p class="border-b border-white border-dashed border-opacity-50">Wading Birds Sound System</p>
-			<p class="border-b border-white border-dashed border-opacity-50">Roots of joy</p>
-			<p class="border-b border-white border-dashed border-opacity-50">I-Bredda</p>
+			<span class="text-sm -translate-y-1">19h-20h15</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Wading Birds Sound System</p>
+			<span class="text-sm -translate-y-1">21h15-22h30</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">Roots of joy</p>
+			<span class="text-sm -translate-y-1">23h30-00h30</span>
+			<p class="border-b font-semibold border-white border-dashed border-opacity-50">I-Bredda</p>
 		</div>
 	</a>
 </div>
